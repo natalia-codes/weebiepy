5b04cf6fb7bfdb20e7acd0a59732a192e88bdd78
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-01-26 19:30:04 -0800

    token storage front end

---
+++ client/src/components/useToken.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function useToken() {
+    function getToken() {
+        const userToken = localStorage.getItem('token');
+        return userToken && userToken
+    }
+
+    const [token, setToken] = useState(getToken());
+
+    function saveToken(userToken) {
+        localStorage.item('token', userToken);
+        setToken(userToken);
+    };
+    function removeToken() {
+        localStorage.removeItem('token');
+        setToken(null);
+    }
+    return {
+        setToken: saveToken,
+        token,
+        removeToken
+    }
+}
+
+export default useToken;
